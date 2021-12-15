2b96cec367e87fd2095099d74c74c8ef55650be6
Mewadi11 <thisisthemay@gmail.com>
2021-12-15 08:12:01 +0700

    ทำเมนูโชว์ได้แล้วเวลากดแฮม

--- app/js/script.js
+++ app/js/script.js
@@ -1,33 +1,28 @@
 const btnHamburger = document.querySelector('#btnHamburger');
-const body = document.querySelector('.body');
+const body = document.querySelector('body');
 const header = document.querySelector('.header');
-const overlay = document.querySelector('.overlay')
+const overlay = document.querySelector('.overlay');
 const fadeElems = document.querySelectorAll('.has-fade');
 
-
 btnHamburger.addEventListener('click', function(){
-    console.log('click hamburger');
+  console.log('click hamburger');
 
-    if(header.classList.contains('open')){
-        // body.classList.remove('noscroll');
-        header.classList.remove('open');
-        fadeElems.forEach(function(element){
-            element.classList.remove('fade-in');
-            element.classList.add('fade-out');
-        });
-        
-    }
-    else { //เปิดเมนูแฮม//
-        //  body.classList.add('noscroll');
-        header.classList.add('open');
-        fadeElems.forEach(function(element){
-            element.classList.remove('fade-out');
-            element.classList.add('fade-in'); 
-        });
-    }
+  if(header.classList.contains('open')){ // Close Hamburger Menu
+    body.classList.remove('noscroll');
+    header.classList.remove('open');    
+    fadeElems.forEach(function(element){
+      element.classList.remove('fade-in');
+      element.classList.add('fade-out');
+    });
     
-});
+  }
+  else { // Open Hamburger Menu
+    body.classList.add('noscroll');
+    header.classList.add('open');
+    fadeElems.forEach(function(element){
+      element.classList.remove('fade-out');
+      element.classList.add('fade-in');
+    });
 
-document.querySelector('#btn').addEventListener('click', function () {
-    console.log('test');
-})
+  }  
+});
